d218499837c14ee53d5b4e89930910d93d03ed13
Vitor Hirt <vitordev.webdeveloper@outlook.com>
2025-09-29 23:45:06 -0300

    Adiciona seção de contato e projetos ao portfólio, incluindo links para redes sociais e melhorias no layout responsivo

---
+++ components/contact.php
@@ -0,0 +1,39 @@
+<div class="contact flex mt-10 justify-center items-center">
+    <div class="container p-3 flex flex-col justify-center items-center">
+        <div class="flex justify-center items-center mb-7 flex-col">
+            <h5 class="text-purple-500">Contato</h5>
+            <h1 class="font-bold text-white text-2xl">Gostou do meu trabalho?</h1>
+            <h3 class="text-white font-normal font-bold ">Entre em contato ou acompanhe as minhas redes sociais!</h3>
+        </div>
+        <div class="flex w-1/4 flex-col gap-2">
+            <a class="flex w-full rounded-md bg-green-200 p-3  justify-between items-center" href="#">
+                <div class="flex  items-center gap-2">
+                    <i class="fa-brands fa-github"></i>
+                    <span>Github</span>
+                </div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+            </a>
+            <a class="flex w-full rounded-md bg-green-200 p-3  justify-between items-center" href="#">
+                <div class="flex items-center gap-2">
+                    <i class="fa-brands fa-github"></i>
+                    <span>LinkedIn</span>
+                </div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+            </a>
+            <a class="flex w-ful rounded-md bg-green-200 p-3 justify-between items-center" href="#">
+                <div class="flex items-center gap-2">
+                    <i class="fa-brands fa-github"></i>
+                    <span>Facebook</span>
+                </div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+            </a>
+            <a class="flex w-full rounded-md bg-green-200 p-3 justify-between items-center" href="#">
+                <div class="flex items-center gap-2">
+                    <i class="fa-brands fa-github"></i>
+                    <span>Github</span>
+                </div>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+            </a>
+        </div>
+    </div>
+</div>
--- components/project.php
+++ components/project.php
@@ -1,9 +1,40 @@
-<div>
-    <div class="flex justify-center items-center flex-col">
-        <h3 class="text-red-400 font-normal text-1xl font-bold">Meu Trabalho</h3>
-        <h1 class="text-white font-bold text-2xl">Veja os projetos em destaque</h1>
-    </div>
-    <div>
-
+<div class="flex justify-center items-center">
+    <div class="container justify-center items-center">
+        <div class="flex justify-center items-center mb-7 flex-col">
+            <h3 class="text-red-400 font-normal text-1xl font-bold">Meu Trabalho</h3>
+            <h1 class="text-white font-bold text-2xl">Veja os projetos em destaque</h1>
+        </div>
+        <div class="flex project-flex p-2 justify-center gap-4 items-center">
+            <div class="project flex rounded-xl p-5  justify-center gap-4 items-center bg-cyan-200">
+                <div class="flex w-full  justify-center items-center h-auto">
+                    <img class="rounded-xl h-auto" src="https://placehold.co/600x400" alt="Projeto 1">
+                </div>
+                <a href="#">
+                    <h3 class="text-white font-bold text-1xl">Projeto 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl sit amet.</p>
+                    <div class="mt-5 flex gap-2 flex-wrap">
+                        <span class="w-auto font-bold text-center bg-purple-500 p-2 rounded-lg">PHP</span>
+                        <span class="w-auto font-bold text-center bg-blue-500 p-2 rounded-lg">HTML</span>
+                        <span class="w-auto font-bold text-center bg-red-500 p-2 rounded-lg">CSS</span>
+                        <span class="w-auto font-bold text-center bg-yellow-500 p-2 rounded-lg">Javascript</span>
+                    </div>
+                </a>
+            </div>
+            <div class="project flex rounded-xl p-5  justify-center gap-4 items-center bg-cyan-200">
+                <div class="flex w-full  justify-center items-center h-auto">
+                    <img class="rounded-xl h-auto" src="https://placehold.co/600x400" alt="Projeto 1">
+                </div>
+                <a href="#">
+                    <h3 class="text-white font-bold text-1xl">Projeto 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl sit amet.</p>
+                    <div class="mt-5 flex gap-2 flex-wrap">
+                        <span class="w-auto font-bold text-center bg-purple-500 p-2 rounded-lg">PHP</span>
+                        <span class="w-auto font-bold text-center bg-blue-500 p-2 rounded-lg">HTML</span>
+                        <span class="w-auto font-bold text-center bg-red-500 p-2 rounded-lg">CSS</span>
+                        <span class="w-auto font-bold text-center bg-yellow-500 p-2 rounded-lg">Javascript</span>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>

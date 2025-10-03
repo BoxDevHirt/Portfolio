befae9463955998f7c79fca2e5e899a343ac2680
Vitor Hirt <vitordev.webdeveloper@outlook.com>
2025-10-02 22:23:35 -0300

    Finalizando portfolio 2.0

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-    <p>&copy; <?php date('Y'); ?> | Todos os direitos reservados ao proprietário do site.</p>
+    <p>&copy; <?php echo date('Y'); ?> | Todos os direitos reservados ao proprietário do site.</p>
 </div>
